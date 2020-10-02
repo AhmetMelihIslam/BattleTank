072e2ff97fddbc57df7bbd9b1af77fa72de32fa4
DESKTOP-MUTGVN5\TarmaDSho <ahmetmelihislam@gmail.com>
2020-10-02 18:43:25 +0300

    Creating an AI Controller Class

--- BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBattleTank_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB6E7B369,
-				0xD9731E18,
+				0x7F0BF22F,
+				0x6F0C16FB,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
