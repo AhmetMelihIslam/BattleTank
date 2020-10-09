9714034f573af0e647747157c6c5598f7d8e1b17
DESKTOP-MUTGVN5\TarmaDSho <ahmetmelihislam@gmail.com>
2020-10-09 17:44:39 +0300

    Review Our Execution Flow

--- BattleTank/Source/BattleTank/Private/TankAiminngComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAiminngComponent.cpp
@@ -33,6 +33,13 @@ void UTankAiminngComponent::AimAt(FVector HitLocation,float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f Aim solution found"), Time);
+	}
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f No aim solve found"), Time);
 	}
 
 }
